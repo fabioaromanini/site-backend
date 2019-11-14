48542f49b896c51cc658ce2b3afb5412f4cffe09
Fabio Romanini <fabioaromanini@gmail.com>
2019-11-14 20:24:27 -0200

    retrieves data from projects table in get

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 'use strict';
+const { DynamoDB } = require('aws-sdk');
+
+const dynamodbClient = new DynamoDB.DocumentClient();
+const { PROJECTS_DYNAMO_DB_TABLE } = process.env;
 
 exports.get = async (event, context) => {
+  const response = await dynamodbClient
+    .scan({ TableName: PROJECTS_DYNAMO_DB_TABLE })
+    .promise();
+
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: `Hello, the current time is ${new Date().toTimeString()}.`,
-    }),
+    body: JSON.stringify(response.Items),
   };
 };
